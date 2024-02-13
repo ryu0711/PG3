55693d1d58bd2c6289494ce591faf45e205aed18
平塚琉星 <@MSI>
2024-02-13 23:17:37 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<thread>
+
+void Thread1() {
+	
+	printf("thread1\n");
+};
+void Thread2() {
+	
+	printf("thread2\n");
+};
+void Thread3() {
+	
+	printf("thread3\n");
+};
+int main() {
+	std::thread th1(Thread1);
+	th1.join();
+	std::thread th2(Thread2);;
+	th2.join();
+	std::thread th3(Thread3);
+	th3.join();
+	return 0;
+}
