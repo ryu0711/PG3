c66c3ef3e3932cc3b914d2af4f3ca74d1f0f6eee
平塚琉星 <@MSI>
2024-02-13 22:55:45 +0900

    プロジェクト ファイルを追加します。

---
+++ ICommand.cpp
@@ -0,0 +1,15 @@
+﻿#include "ICommand.h"
+#include"Player.h"
+ICommand::~ICommand()
+{
+}
+
+void RightCommand::Exec(Player& player)
+{
+	player.MoveRight();
+}
+
+void LeftCommand::Exec(Player& player)
+{
+	player.MoveLeft();
+}
